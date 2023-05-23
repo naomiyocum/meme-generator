bed67caae864addb71eebeaa4461aede943a2094
Naomi Yocum <naomiyocum24@gmail.com>
2023-05-23 12:28:10 -0600

    add state to top and bottom texts

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -19,6 +19,14 @@ export default function Meme() {
     }))
   }
 
+  function handleChange(event) {
+    const {name, value} = event.target
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      [name]: value
+    }))
+  }
+
   return (
     <main>
       <div className="meme--form">
@@ -26,11 +34,17 @@ export default function Meme() {
           type="text"
           className="form--input"
           placeholder="Top text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
         />
         <input
           type="text"
           className="form--input"
           placeholder="Bottom text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
         />
         <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼
@@ -38,8 +52,8 @@ export default function Meme() {
       </div>
       <div className="meme">
         <img src={meme.randomImage} className="meme--image" alt="Random Meme" />
-        <h2 className="meme--text top">Hello</h2>
-        <h2 className="meme--text bottom">World</h2>
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
     </main>
   )
